18ba905ee4a6c7f4e8c67a959e5b69d52cc3ccce
CorCor67 <co68956@gmail.com>
2011-09-06 05:56:18 -0500

    [ApkManagerLauncher] update...

--- scripts/ApkManagerLauncher.sh
+++ scripts/ApkManagerLauncher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-# ApkManager Launcher v1.0
-# Created by CorCor67
-# v1.0 Original build
+# ApkManager Launcher v2.0
+# by CorCor67
+# v2.0 TheNeXGen
 
-gnome-terminal  --execute bash -c "cd /home/$USER/Android/ApkManager && /home/$USER/Android/ApkManager/Script.sh && exit ; bash"
+gnome-terminal  --execute bash -c "cd ~/ThemeR-Studio_V2/Android/ApkManager && ~/ThemeR-Studio_V2/Android/ApkManager/Script.sh && exit ; bash"
 exit 0 
