20996d7925dea7b502d254ac26795e412f586af5
CorCor67 <co68956@gmail.com>
2012-02-28 21:31:30 -0600

    Remove ApkManagerLauncher.sh...don't feel it's needed and has been broke for some time with no complaints from anyone else

--- scripts/ApkManagerLauncher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# ApkManager Launcher v2.0
-# by CorCor67
-# v2.0 TheNeXGen
-
-gnome-terminal  --execute bash -c "cd ~/ThemeR-Studio_V2/Android/ApkManager && ~/ThemeR-Studio_V2/Android/ApkManager/Script.sh && exit ; bash"
-exit 0 
