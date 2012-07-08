e9e26cfad301c663f2471fa61a146b9f618088a0
CorCor67 <co68956@gmail.com>
2012-07-08 13:10:17 -0500

    Lots of changes

---
+++ scripts/launchandroid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ThemeR Studio v2.0 by CorCor67
+# v2.0 TheNeXGen
+
+# PATHS
+ts=~/ThemeR-Studio_V2
+s=$ts/scripts
+tools=$ts/.tools
+ws=$ts/Android
+
+# Launch Android script from SDK so user can install platforms and tools.
+clear
+echo "A window will open for the Android SDK"
+echo "Install platforms and tools from here or close it to continue."
+
+cd $ws/android-sdk-linux/tools/
+./android
---
+++ scripts/workspace.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# ThemeR Studio v2.0 by CorCor67
+# v2.0 TheNeXGen
+
+# Paths
+ts=~/ThemeR-Studio_V2
+s=$ts/scripts
+tools=$ts/.tools
+ws=$ts/Android
+
+# Setup Directories
+echo "Creating Android workspace..." 
+     mkdir -p $ws
+     mkdir -p $ws/ThemeR-Studio
+     mkdir -p $ws/ApkManager
+     mkdir -p $ws/apkopt
+7z x -o"$ts" $s/tools.7z
+cd $tools  
+chmod u+rwx *
+
+# Setup apkopt
+       cp "$s/apkopt.sh" $ws/apkopt
+
+# Setup ApkManager
+cp "$s/ApkManager.sh" $ws/ApkManager
+cp "$s/ApkManagerICS.sh" $ws/ApkManager
+cp "$s/ThemeR-Studio.sh" $ws/ThemeR-Studio
+
+mkdir -p $ts/.sdkDL
+cd $ts/.sdkDL
+      var_links=`wget -q -L -O - \
+       http://developer.android.com/sdk/index.html \
+       | grep -o 'http://dl.google.com/android/android-sdk[^"]*linux.tgz' \
+       | sed -e 's/<a href=\"//' -e 's/\"/\n/'`
+    wget --spider $var_links
+    wget $var_links 
+tar xvzf $ts/.sdkDL/*tgz -C "$ws"
+rm -rf $ts/.sdkDL
