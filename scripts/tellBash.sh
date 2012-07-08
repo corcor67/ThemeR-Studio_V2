1912bdf92b62a88247198559ca3d479dff8eee39
corcor67 <corcor67@corcor67.(none)>
2012-07-08 10:57:26 -0500

    Complete rewrite of tell bash we're now using a much smarter script to detect existing bashrc files and if they alredy have the paths setup.  We're also backing up the bashrc if the user has an existing file and pushing our path rather than making the user copy/paste.

--- scripts/tellBash.sh
+++ scripts/tellBash.sh
@@ -2,17 +2,35 @@
 # Edit Bash v2.0
 # by CorCor67
 # v2.0 TheNeXGen
+b=~/.bashrc
+c=~/.BU_bashrc
+path='export PATH=$PATH:~s/ThemeR-Studio_V2/Android/android-sdk-linux/tools:~/ThemeR-Studio_V2/Android/android-sdk-linux/platform-tools:~/bin'
 
-clear
-echo "You need to manually edit your .bashrc to include the paths for the Android SDK"
-echo "Copy & paste the following 3 lines to the text editor that opened,  then go file>save"
-echo ""
-echo ""
-echo "# Path for Android SDK"
-echo 'export PATH=$PATH:~/ThemeR-Studio_V2/Android/android-sdk-linux/tools:~/ThemeR-Studio_V2/Android/android-sdk-linux/platform-tools:~/bin'
-echo ""
-echo ""
-gedit /home/$USER/.bashrc
-echo "Press enter when you are done"
-read enter
-exit
+
+echo "Checking to see if you have an existing .bashrc"
+	if [ -e "$b" ]
+		then
+			f=found
+			echo "You do have an existing .bashrc"
+		else
+			f=not
+			echo "We couldn't find a .bashrc so we'll make one."
+			touch ~/.bashrc
+	fi
+
+echo "Checking if you alredy have ThemeR-Studio paths set up"
+
+	if grep -Fxq "$path" ~/.bashrc
+		then
+			echo "You alredy have the correct paths!"
+		else
+			echo "Couldn't find the paths, no problem, we will add them."
+if [ $f == found ]
+then
+			echo "Backing up current .bashrc as .BU_bashrc"
+			cp $b $c
+fi
+			echo "Adding paths for ThemeR-Studio to .bashrc"
+			echo $path >> $b
+	fi
+
